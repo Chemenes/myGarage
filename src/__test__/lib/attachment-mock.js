125dc0b8ea2412e00d650e200b54c117d3f113b6
Tracy Williams <selpilot@gmail.com>
2018-07-09 09:14:41 -0700

    attachment-mock and model built

---
+++ src/__test__/lib/attachment-mock.js
@@ -0,0 +1,35 @@
+
+import faker from 'faker';
+import Attachment from '../../model/attachment';
+
+import { createProfileMockPromise, removeAllResources } from './profile-mock';
+
+const createAttachmentMockPromise = async () => {
+  const mockData = {};
+
+  const mockProfileData = await createProfileMockPromise();
+
+  mockData.account = mockProfileData.account;
+  mockData.profile = mockProfileData.profile;
+  mockData.token = mockProfileData.token;
+
+  const attachment = await new Attachment({
+    originalName: faker.system.fileName(),
+    mimeType: 'mime/type',
+    encoding: 'utf-8',
+    url: faker.random.image(),
+    awsKey: 'multer-hashed-filename.originalName',
+    profileId: mockData.profile._id,
+  }).save();
+
+  mockData.attachment = attachment;
+
+  const removeAttProfAccntMock = () => {
+    return Promise.all([
+      Attachment.remove(),
+      removeAllResources(),
+    ]);
+  };
+};
+
+export { createAttachmentMockPromise, removeAttProfAccntMock };
